811501bc50fa06a3e3d663c33356eeacd3d60009
N.S. Lin <twelendil@gmail.com>
2012-12-21 08:42:25 +0800

    add PayUnit

---
+++ pay_unit.h
@@ -0,0 +1,14 @@
+#ifndef PAY_UNIT_H_INCLUDED
+#define PAY_UNIT_H_INCLUDED
+
+#include "map_unit.h"
+
+class PayUnit : public MapUnit{
+public:
+    virtual int stop_cost() = 0;
+    virtual int buy_cost() = 0;
+protected:
+    int _cost_;
+};
+
+#endif // PAY_UNIT_H_INCLUDED
