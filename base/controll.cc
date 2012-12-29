70c1596ae582f8e230594655a5416a3d144122d2
N.S. Lin <twelendil@gmail.com>
2012-12-29 10:16:23 +0800

    add base/controll.h&.cc

---
+++ base/controll.cc
@@ -0,0 +1,48 @@
+#include "base/controll.h"
+#include <cstdio>
+#include <cstdlib>
+
+#ifdef __unix__
+ #include <termios.h>
+ #include <unistd.h>
+int unix_getch(){
+    struct termios oldt, newt;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldt );
+    newt = oldt;
+    newt.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newt );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
+    return ch;
+}
+#endif
+
+void Pause(){
+#ifdef __unix__
+    printf("Press Enter key to continue...");
+    unix_getch();
+  #else
+    #ifdef _WIN32
+    system("pause");
+    #else
+    printf("Press \"Enter\" to continue...");
+    scanf("%*c");
+    #endif
+#endif
+}
+
+void Clear(){
+    #ifdef __unix__
+    system("clear");
+  #else
+    #ifdef _WIN32
+    system("cls");
+    #else
+    static const int kHeight = 24;
+    for(int i = 0 ; i < kHeight ; ++i) {
+        putchar('\n');
+    }
+    #endif
+#endif
+}
---
+++ base/controll.h
@@ -0,0 +1,7 @@
+// Copyright 2012 N.S.Lin @ CSEI.NTNU@Taiwan
+
+// wait for any key
+void Pause();
+
+// clear screen
+void Clear();
