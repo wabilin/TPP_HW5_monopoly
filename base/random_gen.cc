93fdcbff0c99d8131d977efba2ad93c73b8a5933
N.S. Lin <twelendil@gmail.com>
2013-01-01 12:53:36 +0800

    base/random_gen fixed for mingw

--- base/random_gen.cc
+++ base/random_gen.cc
@@ -1,14 +1,31 @@
 // Copyright 2012 N.S.Lin @ CSEI.NTNU@Taiwan
 #include "base/random_gen.h"
-#include <random>
 
+
+#ifndef __MINGW32__
+#include <random>
 RandomGen::RandomGen(int min, int max)
   : gen_(std::random_device()()),
     distribution_(std::uniform_int_distribution<int> (min, max))
     {}
 
-RandomGen::~RandomGen() {}
 
 int RandomGen::operator()() {
     return distribution_(gen_);
 }
+
+#else
+#include <cstdlib>
+#include <ctime>
+
+RandomGen::RandomGen(int min, int max)
+  : kRange_(max-min+1), kMin_(min) {
+    srand(time(NULL))
+}
+
+
+int RandomGen::operator()() {
+    return rand()%kRange_ + kMin_;
+}
+
+#endif
--- base/random_gen.h
+++ base/random_gen.h
@@ -2,20 +2,27 @@
 #ifndef BASE_RANDOM_GEN_H_
 #define BASE_RANDOM_GEN_H_
 
-#include <random>
+// MinGW 4.6 do not support <random> as GCC do
+#ifndef __MINGW32__
+  #include <random>
+#endif
 
 class RandomGen {
  public:
     // init with random range
     explicit RandomGen(int min, int max);
-    ~RandomGen();
 
     // return a random number
     int operator()();
 
  private:
+#ifndef __MINGW32__
     std::mt19937 gen_;
     std::uniform_int_distribution<int> distribution_;
+#else
+    const int kRange_;
+    const int kMin_;
+#endif
 };
 
 #endif  // BASE_RANDOM_GEN_H_
