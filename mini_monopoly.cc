884bcf3a9c0aa4775e6d9eaf1a1609922709da7c
N.S. Lin <twelendil@gmail.com>
2013-01-01 11:39:01 +0800

    mini_monopoly.cc done

--- mini_monopoly.cc
+++ mini_monopoly.cc
@@ -1,17 +1,33 @@
 // Copyright 2012 N.S.Lin @ CSEI.NTNU@Taiwan
 
 #include <cstdio>
+#include <cstring>
 #include "game/game.h"
 
-#include "base/random_gen.h"
+// get filepath from command, or use default
+FILE* GetFile(int argc, char** argv) {
+    const char* filepath = "map.dat"; // default
+    for (int i = 0 ; i < argc - 1 ; ++i) {
+        if(strcmp(argv[i], "-f") == 0) {
+            filepath = argv[i+1];
+        }
+    }
+
+    FILE* fptr = fopen(filepath, "r");
+    if (!fptr) {
+        fprintf(stderr, "Error at MapFile: can not open file: %s\n", filepath);
+        exit(EXIT_FAILURE);
+    }
+    return fptr;
+}
 
 int main(int argc, char** argv) {
+    FILE* map_file = GetFile(argc, argv);
 
-    FILE* fptr = fopen("map.dat", "r");
+    Game monopoly;
+    monopoly.InitGame(map_file);
+    monopoly.MainLoop();
 
-    Game g;
-    g.InitGame(fptr);
-    g.MainLoop();
-
-    return 0;
+    return EXIT_SUCCESS;
 }
+
